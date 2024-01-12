c6f40b808799f678d9288b695c216fd09e65f905
Noam <85963641+piscnoam@users.noreply.github.com>
2024-01-12 18:26:16 -0400

    imported the cyberfu things

---
+++ passion project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentPlayerHealth = 15;
+    public int enemyDamage = 3;
+
+    public PlayerExplosionParticles particles;
+    private Animator playerAnimator;
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
+    }
+    
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+
+        if(currentPlayerHealth <= 0)
+        {
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "HitCollider")
+        {
+            HurtPlayer();
+        }
+    }
+}
