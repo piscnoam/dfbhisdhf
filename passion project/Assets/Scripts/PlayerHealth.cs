1db601eb3d5d9a39bf3b83e38071dda467b66e06
Noam <85963641+piscnoam@users.noreply.github.com>
2024-02-19 17:22:41 -0400

    finished it up
    lmk if theres anything to fix

--- passion project/Assets/Scripts/PlayerHealth.cs
+++ passion project/Assets/Scripts/PlayerHealth.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentPlayerHealth = 15;
     public int enemyDamage = 3;
+    public Text deathText;
+    public GameObject menu;
+    public GameObject score;
+    public GameObject hp;
 
     public PlayerExplosionParticles particles;
     private Animator playerAnimator;
     void Start()
     {
+        score.SetActive(false);
+        hp.SetActive(false);
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        deathText.text = "";
         playerAnimator = GetComponent<Animator>();
         particles = GetComponent<PlayerExplosionParticles>();
     }
@@ -24,13 +34,14 @@ public class PlayerHealth : MonoBehaviour
         if(currentPlayerHealth <= 0)
         {
             particles.Explode();
-            Invoke("Death", 1.5f);
+            deathText.text = "you died :(";
+            Invoke("Death", 2f);
         }
     }
 
     private void Death()
     {
-        SceneManager.LoadScene("Dead");
+        Time.timeScale = 0f;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -38,5 +49,12 @@ public class PlayerHealth : MonoBehaviour
         {
             HurtPlayer();
         }
+    }
+
+    public void startButton(){
+        score.SetActive(true);
+        hp.SetActive(true);
+        Time.timeScale = 1f;
+        menu.SetActive(false);
     }
 }
